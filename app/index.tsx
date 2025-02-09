6a028fa2a3e3364a7cfabfac4fa6e8b294c0711b
Dani Matuko <danimatuko@outlook.com>
2025-02-09 22:06:29 +0200

    add assets and gluestack-ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { Heading } from "@/components/ui/heading";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <Heading className="text-center mt-5">Zenflow</Heading>
+      </ImageBackground>
     </View>
   );
 }
